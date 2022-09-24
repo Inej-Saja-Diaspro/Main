50ddbcf5bca92ba60353d5255fab87036cf71ffc
Malena Noemí VallecoRsa <anonimatoenmascaradog21@gmail.com>
2022-09-24 20:22:07 -0300

    update

---
+++ .dist/mimoplayground2.py
@@ -0,0 +1,16 @@
+## Sort customers queue service
+customers = ["Filip", "Miranda", "Aria", "Laurence", "Shahid"]
+
+free_counters = [11, 20, 14, 6, 4]
+
+customers.sort()
+
+free_counters.sort()
+
+customer = customers[0]
+
+counter = free_counters[0]
+
+print(customer)
+print("Visit counter:")
+print(counter)
