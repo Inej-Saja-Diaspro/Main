ac3fef5f4fab9976f53ef22f10d27ab28ae26e25
Inej <anonimatoenmascaradog21@gmail.com>
2022-09-26 20:59:32 -0300

    "added new playground"

---
+++ .dist/mimoplayground3.py
@@ -0,0 +1,8 @@
+average = 86
+attendance = 88
+if average > 85 and attendance > 85:
+    print("You passed with flying colors")
+elif average > 70 or attendance > 70:
+    print("You passed")
+else:
+    print("Try again next time")
