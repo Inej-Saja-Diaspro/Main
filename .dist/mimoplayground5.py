61e40d908adfdf233c66c7c147fafb6e950cf2a3
Inej <anonimatoenmascaradog21@gmail.com>
2022-10-05 00:24:24 -0300

    2 new playgrounds

---
+++ .dist/mimoplayground5.py
@@ -0,0 +1,12 @@
+## Body Temperature
+
+low = 97
+high = 99
+temp = 101.2
+
+if temp <= high and temp >= low:
+    print("You're in healthy range!")
+if temp < low:
+    print("Your temperature is low.")
+if temp > high:
+    print("Your temperature is high.")
---
+++ .dist/mimoplayground6.py
@@ -0,0 +1,18 @@
+## Lake sizes
+
+sizes = [7340, 2117, 22300, 31700, 1679, 1014, 23000, 9910, 685]
+
+largest = max(sizes)
+
+print("Largest lake in sq mi:")
+print(largest)
+
+smallest = min(sizes)
+
+print("10th largest lake in sq mi:")
+print(smallest)
+
+difference = largest - smallest
+
+print("Difference between lakes in sq mi:")
+print(difference)
