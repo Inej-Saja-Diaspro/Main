f239a191cd0946f09e678677866595f0de8c931b
Inej <anonimatoenmascaradog21@gmail.com>
2022-10-19 20:37:47 -0300

    new one for college
    exercise module 1

---
+++ teclab/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+            <title>Mi primera aplicación Web</title>
+    </head>
+    <body>
+        <h1>Hola esta es mi primera aplicación Web con PHP</h1>
+        <br><br>
+        <?php echo “La suma de 2 + 8 es: ”.(2 + 10) ?>
+    </body>
+
+</html>
