d3b8b44ba9a0a1bb6be038fbd893afd2304e55da
Inej <anonimatoenmascaradog21@gmail.com>
2022-10-22 16:22:23 -0300

    Second try
    why the fuck that last round count?

--- .dist/mimoplayground10.py
+++ .dist/mimoplayground10.py
@@ -27,3 +27,37 @@ while size_1 != 0:
     print(rounds(a = size_1))
     lost = team_1.pop()
     size_1 = len(team_1)
+
+## test
+print("second try")
+
+team_3 = ["Mia", "Lisa"]
+team_4 = ["James", "Ty", "Ava"]
+
+size_3 = len(team_3)
+size_4 = len(team_4)
+
+def game2(b):
+    size_3 = len(team_3)
+    size_4 = len(team_4)
+    if size_3 != size_4:
+        print("Teams must have the same size!")
+    else:
+        print("Game on!")
+
+print(game2(b = size_3))
+
+team_3 = ["Mia", "Lisa", "Liam"]
+
+print(game2(b = size_3))
+
+def rounds2(a):
+    size_3 = len(team_3)
+    if size_3 != 1:
+        print(f"Rounds left: {size_3}")
+    else:
+        print("Final round")
+
+while size_3 != 0:
+    print(rounds2(a = size_3))
+    lost = team_3.pop()
