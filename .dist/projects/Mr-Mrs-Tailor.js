786420c36865f3bccba712939fefdfdff19a49b7
Inej <anonimatoenmascaradog21@gmail.com>
2022-11-10 21:45:55 -0300

    this too

---
+++ .dist/projects/Mr-Mrs-Tailor.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    alert("Listo")
+});
+
+$("#btn_main").click(function(){
+    window.location.replace("https://github.com/Inej-Saja-Diaspro/Main/blob/417dba832cf26d734e05d45863f333189e45f727/.dist/projects/Mr-Mrs-Tailor-webpage.html");
+})
+
+$("#btn_regis").click(function(){
+    window.location.replace("https://github.com/Inej-Saja-Diaspro/Main/blob/417dba832cf26d734e05d45863f333189e45f727/.dist/projects/Mr-Mrs-Tailor-register.html");
+})
