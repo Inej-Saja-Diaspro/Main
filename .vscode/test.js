2c1ac6375e8238e3eec44ff11852d80b4bf80acf
Inej <anonimatoenmascaradog21@gmail.com>
2022-11-18 16:22:55 -0300

    "wtf now"

---
+++ .vscode/test.js
@@ -0,0 +1,9 @@
+let numero = Math.random()*100;
+numero = Math.round(numero);
+
+let premios = ["common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", "common sword", 
+"rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", "rare sword", 
+"common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", "common character", 
+"rare character", "rare character", "rare character", "rare character", "rare character", "rare character", "rare character", "rare character", "rare character", "rare character", "rare character"]
+
+document.write(premios)
