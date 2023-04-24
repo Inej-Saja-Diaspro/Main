3964ca27dbf87174ee4abfe9b70cf0cd7c561739
Inej <anonimatoenmascaradog21@gmail.com>
2023-04-24 12:49:38 -0300

    "C# and Python folder"

---
+++ .dist/projects/C#/start.cs
@@ -0,0 +1,19 @@
+using System;
+namespace CodeSample{
+    class Program{
+        static void Main(string[] args){
+            int A;
+            int B;
+            int C;
+            int A = 100;
+            int B = 50;
+            int C = -5;
+            int D = B;
+            double E = 40.5;
+            char F = 'a';
+            string G = "This is a test";
+            Console.WriteLine(G);
+            Console.WriteLine(A + B + C + D + E + F);
+        }
+    }
+}
